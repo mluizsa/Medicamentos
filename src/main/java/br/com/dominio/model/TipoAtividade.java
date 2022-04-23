d69bdff4748ace6659133d6cb53d0f741441da9c
mluizsa <mluizsa@gmail.com>
2022-04-23 15:02:19 -0300

    Estudo Java

---
+++ src/main/java/br/com/dominio/model/TipoAtividade.java
@@ -0,0 +1,22 @@
+package br.com.dominio.model;
+
+public enum TipoAtividade {
+	
+	APRESENTACAO("Apresentação"), CURSO("Curso"), MINICURSO("Minicurso"), PALESTRA("Palestra"), SEMINARIO("Seminário"),
+	SIMPOSIO("Simpósio"), OUTRA("Outras");
+	
+	private String descricao;
+
+	TipoAtividade(String descricao){
+		this.descricao = descricao;
+	}
+
+
+	public String getDescricao() {
+		return this.descricao;
+	}
+	
+	
+	
+
+}
